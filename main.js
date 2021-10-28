ff0ea97660edd7414fe5e11fc71231fe377afefe
Arniedude5342 <85857848+Arniedude5342@users.noreply.github.com>
2021-10-27 19:34:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-menu_list_array = ["Veg Margherita Pizza", "Chicken Tandoori Pizza", "Deluxe Veggie Pizza", "Paneer Tikka Pizza", "Veg Extravaganza Pizza", "Veg Supreme Pizza"
- ];
-
-function getmenu(){
-var htmldata;
-    for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-    }
-        document.getElementById("display_menu").innerHTML = htmldata;
-        //give the appropriate id name as display_menu  
-}
-
-function add_item(){
-var htmldata;
-var item = document.getElementById("add_item").value;
-        // use the sort function as - menu_list_array.sort();
-        menu_list_array.sort()
-        htmldata=""
+menu_list_array = ["Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata;
+        htmldata="<ol class='menulist'>"
+        menu_list_array.sort();
         for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+            htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
         }
-         //document.getElementById("display_addedmenu").innerHTML = htmldata;		
-      }
+         htmldata=htmldata+"</ol>"
+         document.getElementById("display_menu").innerHTML = htmldata;
+    }
 
-function add_top(){
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-	add_item();
-}
+    function add_item(){
+        var htmldata;
+        var item=document.getElementById("add_item").value;
+        menu_list_array.push(item);
+        menu_list_array.sort();
+        htmldata="<section class='cards'>"
+        for(var i=0;i<menu_list_array.length;i++){
+
+            htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+        }
+          htmldata=htmldata+"</section>"
+          document.getElementById("display_addedmenu").innerHTML = htmldata;
+    }
